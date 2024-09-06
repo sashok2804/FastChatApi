27f479b9c05584942e79019c24b7b43c58ca8dca
sashok2804 <alex.0111127@gmail.com>
2024-09-07 02:02:31 +0400

    chat 0.1.0

---
+++ FastChatApi/FastChatApi/ChatHub.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+public class ChatHub : Hub
+{
+	// Отправка сообщения в указанную комнату
+	public async Task SendMessage(string room, string user, string message)
+	{
+		Console.WriteLine($"Attempting to send message to room {room}: {message}");
+
+		try
+		{
+			await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+			Console.WriteLine($"Message successfully sent to room {room}");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error sending message: {ex.Message}");
+		}
+	}
+
+
+	// Присоединение к указанной комнате
+	public async Task JoinRoom(string room)
+	{
+		// Проверка на наличие имени комнаты
+		if (string.IsNullOrWhiteSpace(room))
+		{
+			throw new ArgumentException("Room name cannot be null or whitespace.", nameof(room));
+		}
+
+		// Присоединение к группе
+		await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+		// Логирование успешного присоединения
+		Console.WriteLine($"Connection {Context.ConnectionId} joined room {room}");
+	}
+
+	// Покидание указанной комнаты
+	public async Task LeaveRoom(string room)
+	{
+		// Проверка на наличие имени комнаты
+		if (string.IsNullOrWhiteSpace(room))
+		{
+			throw new ArgumentException("Room name cannot be null or whitespace.", nameof(room));
+		}
+
+		// Покидание группы
+		await Groups.RemoveFromGroupAsync(Context.ConnectionId, room);
+
+		// Логирование успешного выхода
+		Console.WriteLine($"Connection {Context.ConnectionId} left room {room}");
+	}
+}
---
+++ FastChatApi/FastChatApi/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Добавление необходимых сервисов
+		builder.Services.AddAuthorization();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSignalR();
+
+
+		// Настройка CORS
+		builder.Services.AddCors(options =>
+		{
+			options.AddPolicy("AllowLocalhost",
+				builder =>
+				{
+					builder.WithOrigins("http://localhost:5238") // Добавьте необходимые домены
+						   .AllowAnyHeader()
+						   .AllowAnyMethod()
+						   .AllowCredentials(); // Разрешение использования куки и авторизации
+				});
+		});
+
+		var app = builder.Build();
+
+
+
+		// Включение маршрутизации
+		app.UseRouting();
+
+		// Использование CORS
+		app.UseCors("AllowLocalhost");
+
+		app.UseAuthorization();
+
+		// Определение маршрутов
+		app.MapHub<ChatHub>("/chathub");
+
+		// Запуск приложения
+		app.Run();
+	}
+}
